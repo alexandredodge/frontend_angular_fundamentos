2e6a8ad5183024a6a9746c34e403e28168022607
alexandredodge <alexandredodge@hotmail.com>
2020-10-01 20:00:35 -0300

    product-delete alteracoes

--- src/app/components/product/product-delete/product-delete.component.ts
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-delete',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDeleteComponent implements OnInit {
 
-  constructor() { }
+  produto: Product;
+
+  constructor(
+      private productService: ProductService,
+      private router: Router,
+      private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.findByIdProdutos(id).subscribe( produtoRet =>{
+      this.produto = produtoRet;
+    })
+  }
+
+  deleteProduct():void{
+    this.productService.delete(this.produto.id).subscribe(()=>{
+      this.router.navigate(['/products']);
+      this.productService.showMessage('Produto excluido com sucesso!')
+    });
+    
+  }
+
+  cancel():void{
+    this.router.navigate(['/products']);
   }
 
 }
