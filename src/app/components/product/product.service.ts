51a73971dbaaab0f30321730f1423fe6afa8345c
Jose Alexandre Oliveira de Souza <jose.osouza@PMCG.IMTI>
2020-10-01 13:51:17 -0400

    product-delete

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -39,7 +39,7 @@ export class ProductService {
 		return this.http.put<Product>(url,product);
 	}
 
-	detele(id:string): Observable<Product>{
+	delete(id:string): Observable<Product>{
 		const url = `${this.baseUrl}/${id}`;
 		return this.http.delete<Product>(url);
 	}
